ef74271699aeccd26bdafeff80faaaf60153a079
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-02-13 13:22:50 +0100

    finished dynamic volumn column for OHLC widget

--- src/widgets/OHLC.jsx
+++ src/widgets/OHLC.jsx
@@ -8,52 +8,17 @@ export function EChart1({data}) {
     const font_color = "#fff"
     const title="OHLC Graph"
     const height = 300
-    const offsetZoomBar = 230
-   
-    if (Object.keys(data).map(key => data[key].length - data.t.length).reduce((a,b) => a+b,0) !== 0) {
-        throw Error("ECharts Candlestick : Given data has not the same length")
-    }
-    const otherColumns = Object.keys(data).filter(x=> !["t","o","c","l","v","h"].includes(x))
     const colorList = ['rgba(155, 89, 182,1.0)', 'rgba(52, 152, 219,1.0)', 'rgba(241, 196, 15,1.0)', 'rgba(232, 67, 147,1.0)', 'rgba(253, 203, 110,1.0)', 'rgba(0, 206, 201,1.0)', 'rgba(162, 155, 254,1.0)', 'rgba(85, 239, 196,1.0)', '#6e7074', '#546570', '#c4ccd3'];
 
-    let volumnSerie = Object.keys(data).includes("v")?[{
-        name: 'Volume',
-        type: 'bar',
-        xAxisIndex: 1,
-        yAxisIndex: 1,
-        data: data.v
-    }]: [];
-
-    let volumnXAxis = Object.keys(data).includes("v")? [{
-        type: 'category',
-        gridIndex: 1,
-        data: data.t,
-        boundaryGap: false,
-        axisLine: { onZero: false },
-        axisTick: { show: false },
-        splitLine: { show: false },
-        axisLabel: { show: false },
-        min: 'dataMin',
-        max: 'dataMax'
-      }]:[]
-
-      let volumnYAxis = Object.keys(data).includes("v")? [{
-          scale: true,
-          gridIndex: 1,
-          splitNumber: 2,
-          axisLabel: { show: false },
-          axisLine: { show: false },
-          axisTick: { show: false },
-          splitLine: { show: false }
-        }]:[]
-
-    let volumnGrid = Object.keys(data).includes("v")? [{
-        left: 0, right: 70, top: height + 120, height: 60
-      }]:[]
-
-    console.log([...volumnXAxis, ...volumnYAxis])
-
-
+    const dataKeys = Object.keys(data)
+    if (dataKeys.map(key => data[key].length - data.t.length).reduce((a,b) => a+b,0) !== 0) {
+        throw Error("ECharts Candlestick : Given data has not the same length")
+    }
+    const otherColumns = dataKeys.filter(x=> !["t","o","c","l","v","h"].includes(x))
+    const volumnExists = dataKeys.includes("v")
+    const offsetZoomBar = volumnExists?250:210
+    const volumnHeight = 40
+    const volumnOffset = 145
 
     const option = {
         animation: true,
@@ -91,7 +56,18 @@ export function EChart1({data}) {
             boundaryGap: false,
             axisLine: { lineStyle: { color: font_color } },
             }, 
-            ...volumnXAxis
+            ...volumnExists? [{
+                type: 'category',
+                gridIndex: 1,
+                data: data.t,
+                boundaryGap: false,
+                axisLine: { onZero: false },
+                axisTick: { show: false },
+                splitLine: { show: false },
+                axisLabel: { show: false },
+                min: 'dataMin',
+                max: 'dataMax'
+              }]:[]
         ],
         dataZoom: [
             { type: 'slider', show: true, xAxisIndex: [0], fillerColor: 'rgba(50,50,50,0.5)',  handleColor: 'rgba(50,50,50,1)'},
@@ -106,9 +82,23 @@ export function EChart1({data}) {
             splitLine: { show: true, lineStyle: { color: "rgba(255,255,255,0.1)" } },
             axisTick: { show: false },
             axisLabel: { inside: true, formatter: '{value}\n'  }
-            }, ...volumnYAxis
+            },
+            ...volumnExists? [{
+                scale: true,
+                gridIndex: 1,
+                splitNumber: 2,
+                axisLabel: { show: false },
+                axisLine: { show: false },
+                axisTick: { show: false },
+                splitLine: { show: false }
+              }]:[]
         ],
-        grid: [ { left: 0, right: 70, top: 60, height: height+60 }, ...volumnGrid ],
+        grid: [
+            { left: 0, right: 70, top: 60, height: height+50 },
+            ...volumnExists? [{
+                left: 0, right: 70, top: height + volumnOffset, height: volumnHeight
+              }]:[]
+        ],
         series: [
             {
             name: 'OHLC',
@@ -135,11 +125,27 @@ export function EChart1({data}) {
                     }
                 }
             }),
-            ...volumnSerie
-        ]
+            ...volumnExists?[{
+                name: 'Volume',
+                type: 'bar',
+                xAxisIndex: 1,
+                yAxisIndex: 1,
+                data: data.t.map((x,i)=> [i, data.v[i], data.c[i]>data.o[i] ? -1:1])
+            }]: []
+        ],
+        ...volumnExists ? {
+            visualMap: {
+                show: true,
+                seriesIndex: dataKeys.findIndex(x=>x=="v"),
+                dimension: 2,
+                pieces: [
+                    { value: -1, color: "rgba(192, 57, 43, 0.5)" },
+                    { value: 1, color: "rgba(39, 174, 96, 0.5)" }
+                ]
+            }
+        }:null
     }
     
-
 
     return <ReactECharts
         option={option}
